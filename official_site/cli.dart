bea1157413f094df6f19dae7a9c2df8688da19be
danntu <dan_ntu@mail.ru>
2026-01-30 12:27:39 +0500

    https://dart.dev/learn/tutorial/interactivity

--- official_site/cli.dart
+++ official_site/cli.dart
@@ -1,6 +1,11 @@
 import 'lib/cli.dart' as cli;
 
+const version = '0.0.1'; // Add this line
+
 void main(List<String> arguments) {
-  print('Hello World: ${cli.calculate()}');
-  print('Hello, Dart!');
+  if (arguments.isEmpty) {
+    print('Hello dart');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
 }
